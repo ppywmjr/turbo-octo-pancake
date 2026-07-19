055fbd9eb72e964f6a3dca0170ed7ff0d5df0e73
michael rust <ppywmjr@gmail.com>
2026-07-19 21:17:24 +0100

    feat: site header

--- app/components/templates/SiteHeader.tsx
+++ app/components/templates/SiteHeader.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from 'next/navigation'
 import { useAuth, Show, UserButton } from '@clerk/nextjs'
+import { PageHeading } from '@/app/components/atoms/text'
 
 export default function SiteHeader() {
   const pathname = usePathname()
@@ -11,9 +12,14 @@ export default function SiteHeader() {
   if (pathname === '/' && !isSignedIn) return null
 
   return (
-    <header className="flex justify-end items-center p-4 gap-4 h-16">
+    <header className="relative flex items-center sm:justify-center justify-between p-4">
+      <PageHeading className="text-2xl sm:text-3xl lg:text-4xl">
+        Catherine Idalia
+      </PageHeading>
       <Show when="signed-in">
-        <UserButton />
+        <div className="absolute right-4 top-1/2 -translate-y-1/2">
+          <UserButton />
+        </div>
       </Show>
     </header>
   )
